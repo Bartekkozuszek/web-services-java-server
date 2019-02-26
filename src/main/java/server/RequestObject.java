d178db725a9f0baf1d83fc38e1924848eb453efb
Carl Ljungberg <ljungberg125@gmail.com>
2019-02-26 18:52:30 +0100

    clean up request object

--- src/main/java/server/RequestObject.java
+++ src/main/java/server/RequestObject.java
@@ -4,91 +4,8 @@ import java.util.Map;
 
 public class RequestObject {
 
-    private String HTTPMethod;
-    private String urlString;
-    private String version;
-    private String host;
-    private String userAgent;
-    private String contentType;
-    private String accept;
-    private String contentLength;
-    private String body;
-
     private Map<String, String> header;
     private Map<String, String> params;
-    //private Map<String, String> body;
-
-    public String getHTTPMethod() {
-        return HTTPMethod;
-    }
-
-    public void setHTTPMethod(String HTTPMethod) {
-        this.HTTPMethod = HTTPMethod;
-    }
-
-    public String getHost() {
-        return host;
-    }
-
-    public void setHost(String host) {
-        this.host = host;
-    }
-
-    public String getUserAgent() {
-        return userAgent;
-    }
-
-    public void setUserAgent(String userAgent) {
-        this.userAgent = userAgent;
-    }
-
-    public String getContentType() {
-        return contentType;
-    }
-
-    public void setContentType(String contentType) {
-        this.contentType = contentType;
-    }
-
-    public String getAccept() {
-        return accept;
-    }
-
-    public void setAccept(String accept) {
-        this.accept = accept;
-    }
-
-    public String getContentLength() {
-        return contentLength;
-    }
-
-    public void setContentLength(String contentLength) {
-        this.contentLength = contentLength;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    public String getUrlString() {
-        return urlString;
-    }
-
-    public void setUrlString(String urlString) {
-        this.urlString = urlString;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
 
     public Map<String, String> getHeader() {
         return header;
@@ -105,12 +22,4 @@ public class RequestObject {
     public void setParams(Map<String, String> params) {
         this.params = params;
     }
-
-//    public Map<String, String> getBody() {
-//        return body;
-//    }
-//
-//    public void setBody(Map<String, String> body) {
-//        this.body = body;
-//    }
 }
